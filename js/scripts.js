e8e3fe516d1c1bf4b6ca40cb24fcfedc1fc3e119
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-11 17:20:06 -0800

    add a button

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,34 @@
-let x = 12
+// function fetchShoutInput() {
+//     const shoutInput = document.getElementById("shoutingInput").value;
+//     document.querySelector("span#shoutingInput1").innerText = shoutInput.toUpperCase()
+// }
 
-function fetchShoutInput() {
-  //this is fetching input value for shoutInput
-  const shoutInput = document.getElementById("shoutingInput").value;
-  //this function replace the ID shoutInput1 with the text of shoutInput
-  document.querySelector("span#shoutingInput1").innerText = shoutInput.toUpperCase()
-}
+// function setFormSubmissionEventHandler() {
+//     let form = document.querySelector("form");
+//     form.onsubmit = function (event) {
+//         event.preventDefault();
+//         fetchShoutInput();
+//         document.querySelector("div#shouting").removeAttribute("class");
+//     };
+// };
 
-//this function creates the event handle.
+// window.onload = function () {
+//     setFormSubmissionEventHandler();
+// }
 
-function setFormSubmissionEventHandler() {
-  console.log("Submitform successfully reached.")
-  let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
-    fetchShoutInput();
-    let x = x + 5
-    document.querySelector("div#shouting").removeAttribute("class");
-  };
+window.onload = function () {
+    let textSize = 20;
+    let form = document.querySelector("form");
+    form.onsubmit = function (event) {
+        event.preventDefault();
+
+        const text = document.getElementById("Input").value;
+        textSize = textSize * 2;
+        document.querySelector("span#textInput").style.fontSize = textSize + "px";
+        document.querySelector("span#textInput").innerText = text.toUpperCase();
+
+        document.querySelector("div#text").removeAttribute("class");
+
+    };
+
 };
-
-//on load, run functions.
-
-window.onload = function() {
-  console.log("scripts start executing!");
-  setFormSubmissionEventHandler();
-}
